1befa494e898da6125c64bce1a531d000cc8b306
Arnel T. Lenteria <arnel.lenteia@gmail.com>
2015-06-13 00:13:21 +0800

    added attachment

---
+++ app/models/Event.php
@@ -0,0 +1,10 @@
+<?php
+use LaravelBook\Ardent\Ardent;
+class Event extends Ardent {
+	protected $fillable = ['title', 'description', 'slug', 'start_datetime', 'end_datetime', 'venue'];
+
+  public function attachments()
+  {
+      return $this->morphTo("Attachment", "attachable");
+  }
+}
